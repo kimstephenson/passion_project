b5389d795dc2aa8d9602f76202269db74cb2f579
Kim Stephenson <email": null>
2016-09-16 12:02:57 -0700

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   has_many :genres_users
   has_many :genres, through: :genres_users
   has_many :instruments, through: :instruments_users
+
+  validates :email, uniqueness: true
+  validate :email_format
+
+  private
+  def email_format
+    unless email.match /\w+@\w+.\w+/
+      errors.add(:email, "must be a valid email address")
+    end
+  end
 end
