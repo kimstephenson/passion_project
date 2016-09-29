f802eee0b3782e19b31a8d0bf04fc240f3886930
Kim Stephenson <kmstephe@usc.edu>
2016-09-29 12:44:22 -0700

    Display searched users by distance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,23 @@
+require 'pony'
+
 get '/users' do
-  data = zipcodes_in_range(current_user.zip_code)
-  zip_code_info = data["zip_codes"]
-  @users = []
-  @distances = {}
-  zip_code_info.each do |hash|
-    @users += User.where(zip_code: hash["zip_code"])
-    @distances[hash["zip_code"]] = hash["distance"]
-  end
-  if params[:instrument] != "all instruments"
-    @users = @users.select { |user| user.instruments.include? Instrument.find_by(name: params[:instrument]) }
-  end
-  if params[:genre] != "all genres"
-    @users = @users.select { |user| user.genres.include? Genre.find_by(name: params[:genre]) }
-  end
+  # data = zipcodes_in_range(current_user.zip_code)
+  # zip_code_info = data["zip_codes"]
+  # @users = []
+  # @distances = {}
+  # zip_code_info.each do |hash|
+  #   @users += User.where(zip_code: hash["zip_code"])
+  #   @distances[hash["zip_code"]] = hash["distance"]
+  # end
+  # if params[:instrument] != "all instruments"
+  #   @users = @users.select { |user| user.instruments.include? Instrument.find_by(name: params[:instrument]) }
+  # end
+  # if params[:genre] != "all genres"
+  #   @users = @users.select { |user| user.genres.include? Genre.find_by(name: params[:genre]) }
+  # end
+  @users_distances = search_for_users
   if request.xhr?
-    erb(:'/users/index', layout: false, locals: {users: @user, distances: @distances})
+    erb(:'/users/index', layout: false)
   else
     erb :'/users/index'
   end
@@ -96,4 +99,12 @@ end
 
 delete '/users/:id' do
   #delete your own profile when logged in
+end
+
+get '/users/:id/contact' do
+  @user = User.find(params[:id])
+  #if request.xhr?
+    #display email form on user's page
+  #else
+  erb :'/users/_contact'
 end
--- app/helpers/zipcode_helpers.rb
+++ app/helpers/zipcode_helpers.rb
@@ -2,4 +2,24 @@ helpers do
   def zipcodes_in_range(zipcode)
     JSON.parse(open("https://www.zipcodeapi.com/rest/#{ENV["ZIP_CODE_KEY"]}/radius.json/#{zipcode}/#{params[:distance]}/mile").read)
   end
+
+  def search_for_users
+    data = zipcodes_in_range(current_user.zip_code)
+    zip_code_info = data["zip_codes"]
+    users = []
+    distances = {}
+    zip_code_info.each do |hash|
+      users += User.where(zip_code: hash["zip_code"])
+      distances[hash["zip_code"]] = hash["distance"]
+    end
+    if params[:instrument] != "all instruments"
+    users = users.select { |user| user.instruments.include? Instrument.find_by(name: params[:instrument]) }
+    end
+    if params[:genre] != "all genres"
+    users = users.select { |user| user.genres.include? Genre.find_by(name: params[:genre]) }
+    end
+    users_distances = {}
+    users.each {|user| users_distances[user] = distances[user.zip_code]  }
+    users_distances.sort_by(&:last).to_h
+  end
 end
