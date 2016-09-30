54f77382f5580e2062f2ac8658d2dc560b4c004d
Kim Stephenson <kmstephe@usc.edu>
2016-09-29 17:42:14 -0700

    Finish implementing HTTParty

--- app/helpers/zipcode_helpers.rb
+++ app/helpers/zipcode_helpers.rb
@@ -1,6 +1,10 @@
+require 'httparty'
+
 helpers do
   def zipcodes_in_range(zipcode)
-    JSON.parse(open("https://www.zipcodeapi.com/rest/#{ENV["ZIP_CODE_KEY"]}/radius.json/#{zipcode}/#{params[:distance]}/mile").read)
+    response = HTTParty.get("https://www.zipcodeapi.com/rest/#{ENV["ZIP_CODE_KEY"]}/radius.json/#{zipcode}/#{params[:distance]}/mile")
+    JSON.parse(response.to_json)
+    # JSON.parse(open("https://www.zipcodeapi.com/rest/#{ENV["ZIP_CODE_KEY"]}/radius.json/#{zipcode}/#{params[:distance]}/mile").read)
   end
 
   def search_for_users
